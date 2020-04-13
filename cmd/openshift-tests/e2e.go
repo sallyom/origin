de717f01266634a17037072d17cd5007e4556653
Sally O'Malley <somalley@redhat.com>
2020-04-13 15:27:57 -0400

    Add extended clusteroperators recover from deleted operand test
    e2e to delete clusteroperators' operand ns, ensure cluster recovery,
    that is, all COs are Available=True Degraded=False Progressing=False,
    except "kube-apiserver", deemed healthy with Progressing=True to meet
    e2e time limit.
    
    This test is necessary because deletion of an operand namespace has an effect on another
    clusteroperator's health.  For example, deleting openshift-service-ca namespace causes openshift-console
    to progress, degrade (if service-ca not recovered).  kube-controller-manager co also affects other COs.

--- cmd/openshift-tests/e2e.go
+++ cmd/openshift-tests/e2e.go
@@ -108,7 +108,7 @@ var staticSuites = testSuites{
 				if strings.Contains(name, "Cluster should survive master and worker failure and recover with machine health checks") {
 					return false
 				}
-				return strings.Contains(name, "[Feature:EtcdRecovery]") || strings.Contains(name, "[Feature:NodeRecovery]") || isStandardEarlyTest(name)
+				return strings.Contains(name, "[Feature:EtcdRecovery]") || strings.Contains(name, "[Feature:NodeRecovery]") || strings.Contains(name, "[Feature:ClusterOperatorRecovery]") || isStandardEarlyTest(name)
 
 			},
 			TestTimeout:         60 * time.Minute,
